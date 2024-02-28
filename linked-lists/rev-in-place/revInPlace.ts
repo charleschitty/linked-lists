f3c1c153582a58a4bcf1e9b3a615dcc57ecbea99
Charles Chitty <charlescchitty@gmail.com>
2024-02-27 16:31:19 -0800

    Attempted to solve linked list rev in place

--- linked-lists/rev-in-place/revInPlace.ts
+++ linked-lists/rev-in-place/revInPlace.ts
@@ -4,6 +4,18 @@ import { LLStr, LLNodeStr } from "../common/ll";
 /** reverseInPlace() reverse list in place.*/
 
 function reverseInPlace(lst: LLStr): void {
+  if (lst.head === null) return // adjusts for empty lists
+
+  let current = lst.head
+  while (current != lst.tail){
+    lst.insertAt(lst.length, current.val)
+    current = current?.next
+  }
+
+  // Look at head
+  // Insert at end, the head
+  // Remove with shift, and ensure that loop ends after returned item is old tail
+  // current = current.next
 }
 
 
